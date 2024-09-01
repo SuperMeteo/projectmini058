b7ece652713d1b1f8addf8adc5e16f9d2df5f924
phu <sakayking01@gmail.com>
2024-09-01 15:09:00 +0700

    as

--- src/components/nav.js
+++ src/components/nav.js
@@ -63,7 +63,6 @@ const Navbar = () => {
     <nav className={`navbar navbar-expand-lg ${styles.navbarCustom}`}>
       <div className="container-fluid">
         <Link className="navbar-brand d-flex align-items-center" href="./">
-          <Image src={Logo} alt="Project Logo" width={60} height={60} className="d-inline-block align-text-top mr-1" />
           <span className={styles.navbarBrandText}>My Gu</span>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
